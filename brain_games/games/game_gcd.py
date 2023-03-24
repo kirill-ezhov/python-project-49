fa7b5eea1934d84d8cfea8c4b236eab44627cad5
Ваше Имя <you@example.com>
2023-03-24 17:11:24 +0300

    gcd

---
+++ brain_games/games/game_gcd.py
@@ -0,0 +1,14 @@
+import random
+import math
+
+OUTPUT_TASK = 'Find the greatest common divisor of given numbers.'
+min_number = 1
+max_number = 100
+
+
+def get_game():
+    first_number: int = random.randint(min_number,max_number)
+    second_number: int = random.randint(min_number, max_number)
+    question: str = f'{first_number} {second_number}'
+    correct_answer: int | str = math.gcd(first_number, second_number)
+    return question, str(correct_answer)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+from brain_games.brain_start import run_game
+from brain_games.games import game_gcd
+
+
+def main():
+    run_game(game_gcd)
+
+
+if __name__ == '__main__':
+    main()
